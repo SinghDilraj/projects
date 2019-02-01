67e11d08e81d179edaf16027611e407b1e350c75
Dilraj Singh <dilrajkehra@gmail.com>
2019-02-01 14:40:41 -0600

    Add files via upload

---
+++ precache-manifest.8f2a02a4e85e0700d337f915eb825818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb765386c38191f004c3ce4687032ba",
+    "url": "/static/media/winnipeg.dfb76538.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "088f5d19d300e0e277eb",
+    "url": "/static/js/main.088f5d19.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "088f5d19d300e0e277eb",
+    "url": "/static/css/main.2e549bff.chunk.css"
+  },
+  {
+    "revision": "4602d14f2f408ea21aebecb065dda373",
+    "url": "/index.html"
+  }
+];
